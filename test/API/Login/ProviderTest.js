cf1e1070054c844de804e87132c8f93a9fadc78c
Adam RInghede <adamringhede@live.com>
2015-05-29 17:22:33 +0200

    Mocks facebook api me call for testing

--- test/API/Login/ProviderTest.js
+++ test/API/Login/ProviderTest.js
@@ -5,6 +5,7 @@ var Models = require('../../../lib/Models');
 Models.init('mongodb://localhost/polynect-test')
 
 process.env.POLYNECT_API_PORT = 8090;
+process.env.MOCK_SERVICES = true;
 
 // Start API
 require('../../../lib/API')
@@ -12,14 +13,10 @@ require('../../../lib/API')
 // Clear database
 Models.Player.collection.remove();
 
-var access_tokens = {
-  facebook: 'CAACEdEose0cBAOZARvWUP5JZAq4xxftI8SZA5NWnGZAb9LCgxjDIcPM664mzxu6ucF1363Uu9OnXj8Pt4iW7QsLSpLZBRwPHtS3OZCVBNpkV6pxsD3dmxdR1w95sAU2Kr6r1zsEkINZBPZBO4xyryVnr2VQ5eRcZBmQK2bloZAmIkiSokcF4fxwBA4Wek7ZCCvCy4u5ZBDV40rwZAg3yjjPVm2FT9'
-};
-
 describe('Login with provider', function () {
   it('works with facebook', function (done) {
     request({ method: 'POST', json: true, url: 'http://localhost:8090/login/facebook',
-      body: {access_token: access_tokens.facebook} }, function (err, res, body) {
+      body: {access_token: 'token'} }, function (err, res, body) {
         assert.equal(res.statusCode, 200);
         assert.equal(typeof body.token, 'string');
         assert.equal(typeof body.token_expires, 'string');
