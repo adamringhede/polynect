ae951b5ef2fe208007137af2de4a5a05bc0a6c65
Adam RInghede <adamringhede@live.com>
2015-05-31 09:04:18 +0200

    Changes tests re use references to real game objects

--- test/API/Player/PlayerTest.js
+++ test/API/Player/PlayerTest.js
@@ -14,12 +14,18 @@ require('../../../lib/API')
 
 
 describe('Players POST', function () {
+  var gameId = null;
   before(function (done) {
     // Clear database
-    Models.Player.collection.remove(done);
+    Models.Game.collection.remove(function () {
+      Models.Player.collection.remove(function () {
+        var g = new Models.Game({name: 'TestGame'});
+        gameId = g._id; g.save(done);
+      });
+    });
   })
   it('creates a new player', function (done) {
-    request({ method: 'POST', json: true, url: 'http://localhost:8090/games/213/players',
+    request({ method: 'POST', json: true, url: 'http://localhost:8090/games/'+gameId+'/players',
       body: {username: 'adamringhede@live.com', password: 'password'} }, function (err, res, body) {
         assert.equal(res.statusCode, 200);
         assert.equal(typeof body.token, 'string')
@@ -31,15 +37,23 @@ describe('Players POST', function () {
 
 describe('Players GET', function () {
   var playerId = null;
+  var gameId = null;
   before(function (done) {
-    Models.Player.collection.remove(function () {
-      Models.Player.createWithCredentials('adamringhede@live.com', 'secret', '213', function (err, model) {
-        playerId = model._id; done();
-      })
-    });
+    Models.Game.collection.remove(function () {
+      Models.Player.collection.remove(function () {
+        var g = new Models.Game({name: 'TestGame'});
+        gameId = g._id;
+        g.save(function () {
+          Models.Player.createWithCredentials('adamringhede@live.com', 'secret', gameId, function (err, model) {
+            playerId = model._id;
+            done()
+          })
+        });
+      });
+    })
   })
   it('retrieves a player by id', function (done) {
-    request({ method: 'GET', json: true, url: 'http://localhost:8090/games/213/players/' + playerId},
+    request({ method: 'GET', json: true, url: 'http://localhost:8090/games/'+gameId+'/players/' + playerId},
       function (err, res, body) {
         assert.equal(res.statusCode, 200);
         assert.equal(body.username, 'adamringhede@live.com');
@@ -47,7 +61,7 @@ describe('Players GET', function () {
       });
   });
   it('returns 404 if player can not be found', function (done) {
-    request({ method: 'GET', json: true, url: 'http://localhost:8090/games/213/players/' + mongoose.Types.ObjectId()},
+    request({ method: 'GET', json: true, url: 'http://localhost:8090/games/'+gameId+'/players/' + mongoose.Types.ObjectId()},
       function (err, res, body) {
         assert.equal(res.statusCode, 404);
         done();
