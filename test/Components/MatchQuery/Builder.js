ed03cd548f406d77ff5d54a03adad9f11775fc2d
Adam RInghede <adamringhede@live.com>
2015-06-05 17:35:04 +0200

    Creates an initial version of match query builder

---
+++ test/Components/MatchQuery/Builder.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var Builder = require('../../../lib/Components/MatchQueryBuilder/MatchQueryBuilder');
+
+describe('MatchQueryBuilder', function() {
+
+  describe('getValue', function() {
+    it('can return input values', function() {
+      var builder = new Builder(
+        require('./Configs/Complex'), // Config
+        { y: 'bar' } // Values
+      );
+      var value = builder.getValue(builder.config.attributes.y)
+      assert.equal(value, 'bar');
+    });
+    it('can use a default value if an input value is undefined', function() {
+      var builder = new Builder(
+        require('./Configs/Complex') // Config
+      );
+      var value = builder.getValue(builder.config.attributes.y)
+      assert.equal(value, 'foo');
+    });
+  })
+});
