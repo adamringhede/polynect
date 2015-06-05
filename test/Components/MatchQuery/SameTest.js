9e358416b40c9118ff220b97e205b2272c2cbde1
Adam RInghede <adamringhede@live.com>
2015-06-05 14:27:35 +0200

    Makes the same and interval methods static since there is not need to create instances of the classes.
    They could even be exported directly instead of creating classes. Or will we ever not check the builders' types?

--- test/Components/MatchQuery/SameTest.js
+++ test/Components/MatchQuery/SameTest.js
@@ -1,17 +1,15 @@
 var Same = require('../../../lib/Components/MatchQueryBuilder/Same');
 var assert = require('assert');
 
-var same = new Same();
-
 describe('Same query', function() {
   it('returns a valid mongo query', function() {
-    var q = same.build('x', {type: 'same', value: 'player.x'}, 43);
+    var q = Same.build('x', {type: 'same', value: 'player.x'}, 43);
     assert.deepEqual(q, {
       'x': 43
     })
   })
   it('works with objects and strings', function() {
-    var q = same.build('x', {type: 'same', value: 'player.x'}, {x:23, b:"test"});
+    var q = Same.build('x', {type: 'same', value: 'player.x'}, {x:23, b:"test"});
     assert.deepEqual(q, {
       'x': {
         x: 23,
