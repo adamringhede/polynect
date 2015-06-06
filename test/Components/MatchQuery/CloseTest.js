47e0b885220b229cf85a39583391bf4d55d8ab50
Adam RInghede <adamringhede@live.com>
2015-06-06 12:14:36 +0200

    Tests for match query builder

---
+++ test/Components/MatchQuery/CloseTest.js
@@ -0,0 +1,15 @@
+var Close = require('../../../lib/Components/MatchQueryBuilder/Close');
+var assert = require('assert');
+
+describe('Close query', function() {
+  it('returns a valid mongo query', function() {
+    var q = Close.build('x', {type: 'close', value: 'player.x', distance: 5}, 43);
+    assert.deepEqual(q, {
+      'x': {
+        $lte: 48,
+        $gte: 38
+      }
+    })
+  })
+});
+// TODO Add tests for all faulty options
