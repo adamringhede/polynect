a5d4e08549abd327cc4a8fe5c66aba4c5182c8c9
Adam Ringhede <adamringhede@live.com>
2016-12-20 10:08:20 +0100

    Create test for web hook sender verification

--- test/Models/WebhookTest.js
+++ test/Models/WebhookTest.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const Models = require('../../lib/Models');
-require('../../lib/API/Helpers/Mocks');
+const nock = require('nock')
+const crypto = require('crypto')
 
+require('../../lib/API/Helpers/Mocks');
 
 Models.init();
 
@@ -26,6 +28,26 @@ describe('Webhook', function () {
       done()
     })
   });
+
+  it('is secured with an X-Hub-Signature', function (done) {
+    const url = 'https://test-hooks.polynect.io'
+    const secret = 'my secret'
+    // Set up receiver
+    nock(url).post('/').reply(200, function(uri, requestBody) {
+      const signature = crypto.createHmac('sha256', secret).update(requestBody).digest('hex')
+      assert.equal(this.req.headers["x-hub-signature"], signature)
+      return requestBody;
+    });
+    // Send a hook
+    const webhook = new Models.Webhook();
+    webhook.url = url;
+    webhook.secret = secret;
+    webhook.enabled.match_init = true;
+    webhook.send('match_init', {match: {}}).then((sent) => {
+      assert.ok(sent, "Hook was never received")
+      done()
+    })
+  })
 
 
 });
